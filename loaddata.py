066ab575a62811f7383b178dd193a95e2b4b444d
Edvin Sidebo <edvin@sidebo.se>
2024-11-23 00:19:03 +0100

    WIP 2022 and valdeltagande

--- loaddata.py
+++ loaddata.py
@@ -1,26 +1,43 @@
 #!/usr/bin/env python
 import geopandas as gpd
 from functools import cache
+from typing import Union
 import pandas as pd
 from val import VAL, VAL_GEO
 import logging
+from pathlib import Path
 
 logger = logging.getLogger(__name__)
 
+DATA_DIR = "data"
+VALRESULTAT_DIR = Path(DATA_DIR) / "valresultat"
+VALGEO_DIR = Path(DATA_DIR) / "valgeografi"
+
 SHAPE_FILES = {
     2018: {
-      VAL_GEO.RIKSDAGSVALKRETS: "2018_valgeografi_riksdagsvalkretsar/alla_riksdagsvalkretsar.shp",
-      VAL_GEO.KOMMUNVALKRETS: "2018_valgeografi_kommunvalkretsar/alla_kommunvalkretsar.shp",
-      VAL_GEO.VALDISTRIKT: "2018_valgeografi_valdistrikt/alla_valdistrikt.shp"
+      VAL_GEO.RIKSDAGSVALKRETS: VALGEO_DIR / "2018_valgeografi_riksdagsvalkretsar/alla_riksdagsvalkretsar.shp",
+      VAL_GEO.KOMMUNVALKRETS:   VALGEO_DIR / "2018_valgeografi_kommunvalkretsar/alla_kommunvalkretsar.shp",
+      VAL_GEO.VALDISTRIKT:      VALGEO_DIR / "2018_valgeografi_valdistrikt/alla_valdistrikt.shp"
+    },
+    2022: {
+      # TODO: get new geo files
+      VAL_GEO.RIKSDAGSVALKRETS: VALGEO_DIR / "2018_valgeografi_riksdagsvalkretsar/alla_riksdagsvalkretsar.shp",
+      VAL_GEO.KOMMUNVALKRETS:   VALGEO_DIR / "2018_valgeografi_kommunvalkretsar/alla_kommunvalkretsar.shp",
+      VAL_GEO.VALDISTRIKT:      VALGEO_DIR / "2018_valgeografi_valdistrikt/alla_valdistrikt.shp"
     }
 }
 
 # Election result (filename, sheet name) tuples
 VALRESULTAT_FILES = {
     2018: {
-        VAL.RIKSDAG: ("2018_R_per_valdistrikt.xlsx", "R procent"),
-        VAL.REGION: ("2018_L_per_valdistrikt.xlsx", "L procent"),
-        VAL.KOMMUN: ("2018_K_per_valdistrikt.xlsx", "K procent")
+        VAL.RIKSDAG: (VALRESULTAT_DIR / "2018_R_per_valdistrikt.xlsx", "R procent"),
+        VAL.REGION:  (VALRESULTAT_DIR / "2018_L_per_valdistrikt.xlsx", "L procent"),
+        VAL.KOMMUN:  (VALRESULTAT_DIR / "2018_K_per_valdistrikt.xlsx", "K procent")
+    },
+    2022: {
+        VAL.RIKSDAG: (VALRESULTAT_DIR / "riksdag_2022.csv", None),
+        VAL.REGION: (VALRESULTAT_DIR / "region_2022.csv", None),
+        VAL.KOMMUN: (VALRESULTAT_DIR / "kommun_2022.csv", None),
     }
 }
 
@@ -30,8 +47,9 @@ def load_geoframe(fname: str = SHAPE_FILES[2018][VAL_GEO.VALDISTRIKT]) -> gpd.ge
     return gpd.read_file(fname)
 
 @cache
-def load_dataframe(fname: str, sheet_name: str = None) -> pd.DataFrame:
-    parties = ["V", "S", "MP", "M", "C", "KD", "SD", "FI"]
+def load_dataframe(fname: Union[str, Path], sheet_name: str = None) -> pd.DataFrame:
+    fname = str(fname)
+    parties = ["V", "S", "MP", "M", "C", "KD", "SD"]
     usecols = parties + ["VALDELTAGANDE", "VALDISTRIKTSNAMN"]
     logger.info(f"Loading data frame {fname}")
     if fname.endswith(".xlsx") and sheet_name is not None:
@@ -43,6 +61,8 @@ def load_dataframe(fname: str, sheet_name: str = None) -> pd.DataFrame:
 if __name__ == "__main__":
   # Geoframe
   # df = load_geoframe()
+
+  df = load_dataframe(*VALRESULTAT_FILES[2022][VAL.KOMMUN])
 
   # Valresultat frame
   df = load_dataframe(*VALRESULTAT_FILES[2018][VAL.RIKSDAG])
--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 import plotly.express as px
 import pandas as pd
+import folium
 import pyproj
 from val import VAL, VAL_GEO
 from loaddata import VALRESULTAT_FILES, SHAPE_FILES, load_geoframe, load_dataframe
+import logging
 
-YEAR = 2018
+logger = logging.getLogger(__name__)
+
+YEAR = 2022
 MAKE_VALRESULTAT = False
-MAKE_VALDELTAGANDE = True
+MAKE_VALDELTAGANDE = True # True
 
 # Load valresultat
 parties = ['V', 'S', 'MP'] 
@@ -17,8 +21,13 @@ df_val = pd.concat([
     for val in [VAL.RIKSDAG, VAL.REGION, VAL.KOMMUN]
 ])
 
-df_valresultat = df_val.melt(value_vars=parties, value_name='Procent', id_vars=['Val','VALDISTRIKTSNAMN'] , var_name='Parti') 
-df_valdeltagande = df_val.melt(value_vars='VALDELTAGANDE', id_vars=['Val', 'VALDISTRIKTSNAMN'], value_name='Valdeltagande').drop(columns=['variable'])
+df_valresultat = df_val.melt(value_vars=parties, 
+                             value_name='Procent', 
+                             id_vars=['Val','VALDISTRIKTSNAMN'], 
+                             var_name='Parti') 
+df_valdeltagande = df_val.melt(value_vars='VALDELTAGANDE', 
+                               id_vars=['Val', 'VALDISTRIKTSNAMN'], 
+                               value_name='Valdeltagande').drop(columns=['variable'])
 
 # Load geo frame and change name of column to match above frame
 df_geo = load_geoframe(SHAPE_FILES[YEAR][VAL_GEO.VALDISTRIKT]).rename(columns={'VD_NAMN': 'VALDISTRIKTSNAMN'})
@@ -75,25 +84,30 @@ if MAKE_VALRESULTAT:
 ### VALDELTAGANDE
 if MAKE_VALDELTAGANDE:
     df = df_geo.merge(df_valdeltagande, on='VALDISTRIKTSNAMN')
-    df = df.rename(columns={'VALDISTRIKTSNAMN': 'Valdistrikt'}).set_index('Valdistrikt')
-    
-    # Select Hammarby-Skarpnack districts
-    df = df[(df.KVK_NAMN == "4 Östra Söderort") & (df.index.str.startswith('Skarpn'))]
+    df = df[df["Val"] == "Riksdag"]
 
-    # Make one big plot with all elections
-    fig = px.choropleth(
-        df,
-        geojson=df.geometry,
-        locations=df.index,
-        color='Valdeltagande',
-        facet_row='Val',
-        projection='mercator',
-        title=f"Valdeltagande {YEAR}",
-        height=1600,
-        width=1400
-    )
-    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[1]))
-    # fig.for_each_trace(lambda t: t.update(name=t.name.split("=")[1]))
-    fig.update_geos(fitbounds="locations", visible=False)
-    fig.write_html(f'./docs/all-valdeltagande.html')
-    fig.show()
+    bins = (0., 70., 75., 80., 85., 100)
+    labels = ('<70%', '70-75%', '75-80%', '80-85%', '>85%')
+    colors = ("red", "yellow", "magenta", "cyan", "purple")
+    df["bin"] = pd.cut(df["Valdeltagande"], bins, labels=labels).astype(str)
+    df["color"] = pd.cut(df["Valdeltagande"], bins, labels=colors).astype(str)
+    
+    # Stockholm area
+    kvk_names = ['1 Södermalm-Enskede', '2 Bromma-Kungsholmen',
+       '3 Norrmalm-Östermalm-Gamla Stan', '4 Östra Söderort',
+       '5 Västra Söderort', '6 Yttre Västerort']
+    df = df[df.KVK_NAMN.isin(kvk_names)]
+
+    lat, lon = 59.314391, 18.066062
+    m = folium.Map(location=[lat, lon], zoom_start=11, tiles='CartoDB Positron')
+    folium.GeoJson(df, style_function=lambda feature: {"fillColor": feature["properties"]["color"]},
+                   tooltip=folium.GeoJsonTooltip(fields=['VALDISTRIKTSNAMN'], aliases=[''])).add_to(m)
+    m.save(f"./stockholm-valdeltagande-{YEAR}.html")
+
+
+"""
+Where did the 'bottom' valdistrikt in Skarpnack go? 
+Seems they disappeared in the 2022 data.
+
+Also, running with 2022 data 'valresultat' track yields bananas Procent values.
+"""
