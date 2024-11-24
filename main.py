b7760bb94a314b2c2971e44baa2ef2aeed892ba0
Edvin Sidebo <edvin@sidebo.se>
2024-11-24 21:33:15 +0100

    fix VD codes

--- main.py
+++ main.py
@@ -9,45 +9,50 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-YEAR = 2022
-MAKE_VALRESULTAT = False
-MAKE_VALDELTAGANDE = True # True
+YEAR = 2018
+MAKE_VALRESULTAT = True 
+MAKE_VALDELTAGANDE = False #True # True
 
+map_columns={'VALDISTRIKTSNAMN': 'VD_NAMN',
+             'Vdnamn': 'VD_NAMN',
+             'Lkfv': 'VD_KOD',
+             'VD': 'VD_KOD',
+             'VALDISTRIKTSKOD': 'VD_KOD'}
 # Load valresultat
-parties = ['V', 'S', 'MP'] 
 
 df_val = pd.concat([
     load_dataframe(*VALRESULTAT_FILES[YEAR][val]).assign(Val=val.name.title())
     for val in [VAL.RIKSDAG, VAL.REGION, VAL.KOMMUN]
 ])
+df_val = df_val.rename(columns=map_columns)
 
+# Reminder: 'VD' = Valdistrikt
 
 # Load geo frame and change name of column to match above frame
-df_geo = load_geoframe(YEAR).rename(
-    columns={'VD_NAMN': 'VALDISTRIKTSNAMN',
-             'Vdnamn': 'VALDISTRIKTSNAMN',
-             'Lkfv': 'VALDISTRIKTSKOD'})
-df_geo["VALDISTRIKTSKOD"] = df_geo["VALDISTRIKTSKOD"].astype(int)
-# Change to the right CRS
+df_geo = load_geoframe(YEAR).rename(columns=map_columns)
+# By comparing with valdistriktskod in the 2018 xlsx files, it seems
+# the last 4 digits is the actual code
+df_geo["VD_KOD"] = df_geo["VD_KOD"].map(lambda x: int(x[-4:]))
 
-
-# Take VALDISTRIKTSNAMN from geo frame
-df = df_geo.merge(df_val.drop(columns=["VALDISTRIKTSNAMN"]), on='VALDISTRIKTSKOD')
+# Take VD_NAMN from geo frame
+df = df_geo.merge(df_val.drop(columns=["VD_NAMN"]), on='VD_KOD')
 # Stockholm area
-df = df[df["VALKRETSNAMN"] == "Stockholms kommun"].copy()
+df = df[df["RVK_NAMN"] == "Stockholms kommun"].copy()
 
 
 ### VALRESULTAT 
 if MAKE_VALRESULTAT:
     if YEAR == 2022:
         raise NotImplementedError("2022 files need to calculate percent")
-    df_valresultat = df_val.melt(value_vars=parties, 
+
+    parties = ['V', 'S', 'MP'] 
+    df = df.melt(value_vars=parties, 
                              value_name='Procent', 
-                             id_vars=['Val','VALDISTRIKTSNAMN', 'VALDISTRIKTSKOD'], 
+                             id_vars=['Val','VD_NAMN', 'VD_KOD', 'KVK_NAMN', 'geometry'], 
                              var_name='Parti') 
     # NB: Seems important to create merged from GEO frame and not the other way
     #     around, to make sure the merged is of the right type.
-    df = df.rename(columns={'VALDISTRIKTSNAMN': 'Valdistrikt'}).set_index('Valdistrikt')
+    df = df.rename(columns={'VD_NAMN': 'Valdistrikt'}).set_index('Valdistrikt')
     
     # Select Hammarby-Skarpnack districts
     df = df[(df.KVK_NAMN == "4 Östra Söderort") & (df.index.str.startswith('Skarpn'))]
@@ -94,18 +99,18 @@ if MAKE_VALDELTAGANDE:
     df = df[df["Val"] == "Riksdag"]
     bins = (0., 70., 75., 80., 85., 100)
     labels = ('<70%', '70-75%', '75-80%', '80-85%', '>85%')
-    # Color-blind friendly red palette gradient sequence
-    colors = ['#fcae91', '#fb6a4a', '#de2d26', '#a50f15', '#67000d']
+    # Color-blind friendly heatmap palette gradient sequence
+    colors = ['#ffffcc', '#ffeda0', '#feb24c', '#fd8d3c', '#e31a1c']
+
     df["bin"] = pd.cut(df["VALDELTAGANDE"], bins, labels=labels).astype(str)
     df["color"] = pd.cut(df["VALDELTAGANDE"], bins, labels=colors).astype(str)
-    print(df["color"].value_counts())
     df["Valdeltagande"] = df["VALDELTAGANDE"].map(lambda v: f"{v*1e-2:.1%}")
     
-    lat, lon = 59.314391, 18.066062
+    lat, lon = 59.314391, 18.066062 # Stockholm
     m = folium.Map(location=[lat, lon], zoom_start=11, tiles='CartoDB Positron')
     folium.GeoJson(df, style_function=lambda feature: {"fillColor": feature["properties"]["color"]},
                    tooltip=folium.GeoJsonTooltip(
-                       fields=['VALDISTRIKTSNAMN', 'Valdeltagande'], 
+                       fields=['VD_NAMN', 'Valdeltagande'], 
                        aliases=['', ''])).add_to(m)
     m.save(f"./stockholm-valdeltagande-{YEAR}.html")
 
